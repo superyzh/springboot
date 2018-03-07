18bc9fd0d04dc71fa7b9e2d6cdcb071b58da13e7
superyzh <527419357@qq.com>
2018-03-07 18:22:39 +0800

    add people

---
+++ src/main/java/cn/springboot/dto/People.java
@@ -0,0 +1,35 @@
+package cn.springboot.dto;
+
+/**
+ * Created by pc on 2018/3/7.
+ */
+public class People {
+
+     private Integer id;
+     private String age;
+     private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
